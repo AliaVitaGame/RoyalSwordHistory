cdb7caa4703404e9c89b54943de33e82cbf12617
XondamirAVG <79681989+XondamirAVG@users.noreply.github.com>
2024-04-26 20:06:46 +0500

    UpdatedBoss

--- RoyalSwordStory/Assets/Sources/ScriptsC#/Bosses/Boss1Level.cs
+++ RoyalSwordStory/Assets/Sources/ScriptsC#/Bosses/Boss1Level.cs
@@ -12,12 +12,17 @@ public class Boss1Level : MonoBehaviour
     private bool _isResting = false;
     private float _jumpDuration = 0.5f;
     private float _jumpTimerElapsed = 0f;
-
+    private float _pauseBeforeJump = 2f;
+    private bool _isPausedBeforeJump = false;
+    private float _pauseTimer = 0f;
+    private bool _playerInsideAggroRange = false;
+    private Animator _bossAnimator;
     private void Start()
     {
         _playerTransform = GameObject.FindGameObjectWithTag("Player").transform;
         _stats = GetComponent<BossesStats>();
         _initialPosition = transform.position;
+        _bossAnimator = GetComponentInChildren<Animator>();
     }
 
     private void Update()
@@ -26,14 +31,36 @@ public class Boss1Level : MonoBehaviour
         {
             _jumpTimer += Time.deltaTime;
 
-            if (_jumpTimer >= _jumpCooldown && !_isJumping)
+            if (_jumpTimer >= _jumpCooldown && !_isJumping && !_isPausedBeforeJump)
             {
-                JumpToPlayer();
+                _isPausedBeforeJump = true;
+                _pauseTimer = 0f;
+            }
+
+            if (_isPausedBeforeJump)
+            {
+                _bossAnimator.SetBool("CriticalAttack", false);
+                _bossAnimator.SetBool("Idle", true);
+                _bossAnimator.SetBool("Attack1", false);
+                _bossAnimator.SetBool("Attack2", false);
+                _bossAnimator.SetBool("Run", false);
+                _pauseTimer += Time.deltaTime;
+
+                if (_pauseTimer >= _pauseBeforeJump)
+                {
+                    _isPausedBeforeJump = false;
+                    JumpToPlayer();
+                }
             }
         }
 
         if (_isJumping)
         {
+            _bossAnimator.SetBool("CriticalAttack", true);
+            _bossAnimator.SetBool("Idle", false);
+            _bossAnimator.SetBool("Attack1", false);
+            _bossAnimator.SetBool("Attack2", false);
+            _bossAnimator.SetBool("Run", false);
             _jumpTimerElapsed += Time.deltaTime;
             float t = _jumpTimerElapsed / _jumpDuration;
             transform.position = Vector2.Lerp(_initialPosition, _playerTransform.position, t);
@@ -49,12 +76,46 @@ public class Boss1Level : MonoBehaviour
 
         if (_isResting)
         {
-            // Ничего не делаем
+            _bossAnimator.SetBool("CriticalAttack", false);
+            _bossAnimator.SetBool("Idle", true);
+            _bossAnimator.SetBool("Attack1", false);
+            _bossAnimator.SetBool("Attack2", false);
+            _bossAnimator.SetBool("Run", false);
         }
 
-        if (!_isJumping && !_isResting)
+        if (!_isJumping && !_isResting && !_isPausedBeforeJump && !_playerInsideAggroRange)
         {
+            _bossAnimator.SetBool("CriticalAttack", false);
+            _bossAnimator.SetBool("Idle", false);
+            _bossAnimator.SetBool("Attack1", false);
+            _bossAnimator.SetBool("Attack2", false);
+            _bossAnimator.SetBool("Run", true);
             transform.position = Vector2.MoveTowards(transform.position, _playerTransform.position, _stats.MoveSpeed * Time.deltaTime);
+        }
+        if (_playerInsideAggroRange && !_isResting && !_isPausedBeforeJump)
+        {
+            _bossAnimator.SetBool("CriticalAttack", false);
+            _bossAnimator.SetBool("Idle", false);
+            _bossAnimator.SetBool("Attack1", true);
+            _bossAnimator.SetBool("Run", false);
+
+        }
+        Vector3 direction = _playerTransform.position - transform.position;
+    
+        float scaleDirection = direction.x < 0 ? -2f : 2f;
+
+        transform.localScale = new Vector3(scaleDirection, transform.localScale.y, transform.localScale.z);
+    }
+    private void FixedUpdate()
+    {
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(transform.position, _stats.AggroRange, LayerMask.GetMask("Player"));
+        if (colliders != null && colliders.Length > 0)
+        {
+            _playerInsideAggroRange = true;
+        }
+        else
+        {
+            _playerInsideAggroRange = false;
         }
     }
 
@@ -71,4 +132,5 @@ public class Boss1Level : MonoBehaviour
         yield return new WaitForSeconds(seconds);
         _isResting = false;
     }
+    
 }
