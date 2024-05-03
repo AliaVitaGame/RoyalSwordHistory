e035b3c6f89ecc4e66e5778d6d482eb9b2d16538
louzes <i.mpossible1551@gmail.com>
2024-05-03 23:31:22 +0500

    добавил новую магию
    лазер свперед: проблема с анимацией пока не решена

--- RoyalSwordStory/Assets/Sources/ScriptsC#/Magic/ArmySpawn.cs
+++ RoyalSwordStory/Assets/Sources/ScriptsC#/Magic/ArmySpawn.cs
@@ -1,11 +1,11 @@
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using UnityEngine;
 
 [CreateAssetMenu]
 public class ArmySpawn : MagicManager
 {
     [SerializeField] private List<GameObject> _soldiers;
-    [SerializeField] private int _soldiersCount;
+    [SerializeField] private ParticleSystem _spawnEffect;
     public override void Activate()
     {
         Randomizer();
@@ -18,7 +18,13 @@ public class ArmySpawn : MagicManager
         {
             Vector3 randomPos = new Vector3(Random.Range(5, 9), 0, 0); 
             Vector3 spawnPosition = player.transform.position + randomPos;
+            _spawnEffect.transform.position = spawnPosition;
             Instantiate(_soldiers[i], spawnPosition, Quaternion.identity);
+
+            ParticleSystem spawnEffectInstance = Instantiate(_spawnEffect, spawnPosition, Quaternion.Euler(-90f, 0, 0));
+            spawnEffectInstance.Play();
+            //перепишу код как только появится союзник игрока чтобы как дочерний объект добавить партиклы
+            //таким образом я буду активировать с ног союзника так как при Instantiate спавнится с тела
         }
     }
 }
---
+++ RoyalSwordStory/Assets/Sources/ScriptsC#/Magic/LaserForward.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu]
+public class LaserForward : MagicManager
+{
+    [SerializeField] private GameObject _lightningPrefab;
+    [SerializeField] private LayerMask _enemyLayer;
+
+    public override void Activate()
+    {
+        ShootLightning();
+    }
+
+    private void ShootLightning()
+    {
+        GameObject player = GameObject.FindGameObjectWithTag("Player");
+        Vector3 shootingDirection = player.transform.localScale.x > 0 ? Vector3.right : Vector3.left;
+        Transform shootingPos = player.transform;
+        GameObject lightning = Instantiate(_lightningPrefab, shootingPos.position, Quaternion.identity);
+
+        lightning.transform.localScale = new Vector3(shootingDirection.x, 1f, 1f);
+
+        RaycastHit2D hit = Physics2D.Raycast(shootingPos.position, shootingDirection, Mathf.Infinity, _enemyLayer);
+        if (hit.collider != null)
+        {
+
+            Destroy(hit.collider.gameObject);
+        }
+
+        // анимации тут будут
+
+        Destroy(lightning, 2f);
+    }
+}
